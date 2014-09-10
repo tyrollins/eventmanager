e3103ae9d8c4bb3ed0781e0b5272cdab2e852efa
stoked <tylerrollins@outlook.com>
2014-09-10 08:49:11 -0700

    added loop to filter name

--- lib/eventmanager.rb
+++ lib/eventmanager.rb
@@ -1,7 +1,9 @@
 puts "EventManager Initialized!"
 
 lines = File.readlines "event_attendees.csv"
-lines.each do |line|
+
+lines.each_with_index do |line, index|
+    next if index == 0
     column = line.split(",")
     name = column[2]
     puts name
