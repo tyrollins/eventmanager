db3e7d69992e47ef1d9eec8199630560e764f2dc
stoked <tylerrollins@outlook.com>
2014-09-10 19:59:21 -0700

    added / refactored zip method

--- lib/eventmanager.rb
+++ lib/eventmanager.rb
@@ -1,19 +1,15 @@
 require "csv"
+
+def clean_zip(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
 puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
-
-  if zipcode.nil?
-    zipcode = "00000"
-  elsif
-    zipcode.length < 5
-    zipcode = zipcode.rjust 5, "0"
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  zipcode = clean_zip(row[:zipcode])
 
   puts "#{name} #{zipcode}"
 end
